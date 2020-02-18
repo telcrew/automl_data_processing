76693c624525d5630e7b434d8ef55cf25339e265
telcrew <anton.wagner@gmail.com>
2020-02-18 11:11:08 +0800

    docker push not working

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,5 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import vanilla_project as app
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
